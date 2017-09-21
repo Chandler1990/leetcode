08a9594bc22118eda9dbebce24ae0b56b3f9d532
YangPeng <1124827384@qq.com>
2017-09-21 21:24:54 +0800

    MergeTwoSortedLists

---
+++ 6_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,21 @@
+
+
+#include "isotream"
+class ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x): val(x),next(NULL){}
+}
+class Solution{
+public:
+    ListNode *mergeTwoLists(ListNode* l1, ListNode* l2){
+        ListNode *head = new ListNode(0);
+        ListNode *ptr = head;
+        if(l1->val > l2->val)
+        {
+            ptr =  
+        }
+
+    }
+
+}
